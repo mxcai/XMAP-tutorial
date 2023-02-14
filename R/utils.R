a242e056a26097064228692a99ae9b184af906c2
mxcai <32085863+mxcai@users.noreply.github.com>
2023-02-14 09:50:41 +0800

    Delete R directory

--- R/utils.R
+++
@@ -1,7 +0,0 @@
-comple <- function(allele) {
-  ifelse(allele == "A", "T", ifelse(allele == "T", "A", ifelse(allele == "G", "C", ifelse(allele == "C", "G", allele))))
-}
-
-log_det <- function(X) {
-  return(determinant(X)$modulus[1])
-}
